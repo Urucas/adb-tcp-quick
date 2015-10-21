db0719a43e1e58de885cea682654bf1198686a1c
vrunoa <vruno.alassia@gmail.com>
2015-10-20 23:43:44 -0300

    No more bash, adb-tcp-quick is now a Nodejs cli

--- adb-tcp-quick.sh
+++
@@ -1,9 +0,0 @@
-$(adb start-server)
-ALREADY_CONNECTED=$(adb devices | egrep -o ':5555')
-if [ ! -z $ALREADY_CONNECTED ]; then
-  echo "Already connected"
-  exit 0
-fi
-IP=$(adb shell ip -f inet addr show wlan0 | egrep -o '[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+' | head -n1)
-$(adb tcpip 5555)
-$(adb connect "${IP}":5555)
